b70750531d1e1ba77bb05f4e51b7859c39afc2e9
Mahmoud Abd Elfatah <mahmdmndbd@gmail.com>
2025-04-03 11:12:52 -0700

    Update Contact Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,16 @@ module.exports = withMT({
     "node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "1.5rem",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+    },
     extend: {
       colors: {
         main: "#DB4444",
