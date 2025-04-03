401654c5ec37d2cb75f3681054244943da608ca7
fady alhares <fadyalhares@gmail,com>
2025-04-03 18:04:57 +0200

    Eror404 page

--- src/components/pages/error.jsx
+++ src/components/pages/error.jsx
@@ -10,8 +10,7 @@ const Error = () => {
         <Typography
           variant="h1"
           color="blue-gray"
-          className="mt-10 !text-3xl !leading-snug md:!text-4xl font-mainFont"
-        >
+          className="mt-10 !text-3xl !leading-snug md:!text-4xl font-mainFont">
           Error 404 <br /> It looks like something went wrong.
         </Typography>
         <Typography className=" font-mainFont mt-8 mb-14 text-[18px] font-normal text-gray-500 mx-auto md:max-w-sm">
@@ -21,8 +20,7 @@ const Error = () => {
         <Link as={Link} to="/">
           <Button
             color="gray"
-            className="font-mainFont w-full px-4 md:w-[8rem] bg-main"
-          >
+            className="font-mainFont w-full px-4 md:w-[8rem] bg-main">
             back home
           </Button>
         </Link>
