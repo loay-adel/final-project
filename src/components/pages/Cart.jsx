1b05990ce7ffb7fb1b94682e0a9d1bad7db74eda
Mahmoud Abd Elfatah <mahmdmndbd@gmail.com>
2025-04-07 11:59:11 -0700

    Add To Cart3

--- src/components/pages/Cart.jsx
+++ src/components/pages/Cart.jsx
@@ -1,10 +1,8 @@
-
 import { Link } from "react-router-dom";
 import { IconButton } from "@material-tailwind/react";
 import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
-
 
 
 const Cart = () => {
@@ -21,70 +19,118 @@ const Cart = () => {
         <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
           <div className="mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8">
             <div className="mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl">
-              <table className="w-full text-left">
-                <thead>
-                  {cart.length > 0 &&
-                    <tr className="border-b">
-                      <th className="py-2">Product</th>
-                      <th className="py-2">Price</th>
-                      <th className="py-2">Quantity</th>
-                      <th className="py-2">Subtotal</th>
-                      <th className="py-2">Action</th>
-                    </tr>
-                  }
 
-                </thead>
-                <tbody>
-                  {cart.length > 0 ? (
-                    cart.map((item) => (
-                      <tr key={item.id} className="border-b">
-                        <td className="py-4 flex items-center">
-                          <img className="h-20 w-20 mr-4" src={item.images[0]} alt={item.title} />
-                          {item.title.split(" ").slice(0, 1).join(" ")}
+              <div className="hidden sm:block">
+                <table className="w-full text-left">
+                  <thead>
+                    {cart.length > 0 && (
+                      <tr className="border-b">
+                        <th className="py-2">Product</th>
+                        <th className="py-2">Price</th>
+                        <th className="py-2">Quantity</th>
+                        <th className="py-2">Subtotal</th>
+                        <th className="py-2">Action</th>
+                      </tr>
+                    )}
+                  </thead>
+                  <tbody>
+                    {cart.length > 0 ? (
+                      cart.map((item) => (
+                        <tr key={item.id} className="border-b">
+                          <td className="py-4 flex items-center">
+                            <img className="h-20 w-20 mr-4" src={item.images[0]} alt={item.title} />
+                            {item.title.split(" ").slice(0, 1).join(" ")}
+                          </td>
+                          <td className="py-4">{item.price} EGP</td>
+                          <td className="py-4">
+                            <div className="flex items-center gap-2">
+                              <IconButton color="red" onClick={() => increaseQty(item.id)}>
+                                +
+                              </IconButton>
+                              <h1>{item.quantity}</h1>
+                              <IconButton color="red" onClick={() => decreaseQty(item.id)}>
+                                -
+                              </IconButton>
+                            </div>
+                          </td>
+                          <td className="py-4">{item.subtotal.toFixed(2)} EGP</td>
+                          <td className="py-4">
+                            <button className="text-red-600 hover:underline" onClick={() => removeFromCart(item.id)}>
+                              Remove
+                            </button>
+                          </td>
+                        </tr>
+                      ))
+                    ) : (
+                      <tr>
+                        <td className="flex flex-col items-center justify-center py-20 text-center text-gray-600">
+                          <ShoppingCartIcon className="w-20 h-20 text-red-500 mb-4" />
+                          <h2 className="text-2xl font-semibold mb-2">Your cart is empty</h2>
+                          <p className="mb-6 text-gray-500">Looks like you haven’t added anything to your cart yet.</p>
+                          <Link
+                            to="/product"
+                            className="inline-block bg-red-600 hover:bg-red-700 text-white px-6 py-3 rounded-lg text-sm font-medium transition duration-300"
+                          >
+                            Return to Shop
+                          </Link>
                         </td>
-                        <td className="py-4">{item.price} EGP</td>
-                        <td className="py-4">
+                      </tr>
+                    )}
+                  </tbody>
+                </table>
+              </div>
+
+              <div className="block sm:hidden">
+                {cart.length > 0 ? (
+                  cart.map((item) => (
+                    <div key={item.id} className="flex items-center border-b py-4 space-x-4 hover:bg-gray-50 transition-all duration-200">
+                      <img className="h-16 w-16 object-cover rounded-md shadow-md" src={item.images[0]} alt={item.title} />
+                      <div className="flex-1">
+                        <h3 className="text-lg font-semibold text-gray-800">{item.title.split(" ").slice(0, 2).join(" ")}</h3>
+                        <div className="flex justify-between mt-2 text-sm text-gray-600">
+                          <span>Price: {item.price} EGP</span>
                           <div className="flex items-center gap-2">
-                            <IconButton color="red" onClick={() => increaseQty(item.id)}>
+                            <IconButton color="red" onClick={() => increaseQty(item.id)} size="sm">
                               +
                             </IconButton>
-                            <h1>{item.quantity}</h1>
-                            <IconButton color="red" onClick={() => decreaseQty(item.id)}>
+                            <h1 className="font-medium">{item.quantity}</h1>
+                            <IconButton color="red" onClick={() => decreaseQty(item.id)} size="sm">
                               -
                             </IconButton>
                           </div>
-                        </td>
-                        <td className="py-4">{item.subtotal.toFixed(2)} EGP</td>
-                        <td className="py-4">
-                          <button className="text-red-600 hover:underline" onClick={() => removeFromCart(item.id)}>
+                        </div>
+                        <div className="mt-2 flex justify-between items-center text-sm text-gray-600">
+                          <span>Subtotal: {item.subtotal.toFixed(2)} EGP</span>
+                          <button className="text-red-600 hover:underline font-medium" onClick={() => removeFromCart(item.id)}>
                             Remove
                           </button>
-                        </td>
-                      </tr>
-                    ))
-                  ) : (
-                    <tr>
-                      <td className="flex flex-col items-center justify-center py-20 text-center text-gray-600">
-                        <ShoppingCartIcon className="w-20 h-20 text-red-500 mb-4" />
-                        <h2 className="text-2xl font-semibold mb-2">Your cart is empty</h2>
-                        <p className="mb-6 text-gray-500">Looks like you haven’t added anything to your cart yet.</p>
-                        <Link
-                          to="/product"
-                          className="inline-block bg-red-600 hover:bg-red-700 text-white px-6 py-3 rounded-lg text-sm font-medium transition duration-300"
-                        >
-                          Return to Shop
-                        </Link>
-                      </td>
-                    </tr>
-                  )}
-                </tbody>
-              </table>
+                        </div>
+                      </div>
+                    </div>
+                  ))
+                ) : (
+                  <div className="flex flex-col items-center justify-center py-20 text-center text-gray-600">
+                    <ShoppingCartIcon className="w-20 h-20 text-red-500 mb-4" />
+                    <h2 className="text-2xl font-semibold mb-2">Your cart is empty</h2>
+                    <p className="mb-6 text-gray-500">Looks like you haven’t added anything to your cart yet.</p>
+                    <Link
+                      to="/product"
+                      className="inline-block bg-red-600 hover:bg-red-700 text-white px-6 py-3 rounded-lg text-sm font-medium transition duration-300"
+                    >
+                      Return to Shop
+                    </Link>
+                  </div>
+                )}
+              </div>
+
+
               {cart.length > 0 &&
                 <div className="flex justify-between mt-4">
                   <Link className="px-4 py-2 border border-gray-300 rounded-md" to="/product">Return To Shop</Link>
                   <button className="px-4 py-2 border border-gray-300 rounded-md" onClick={clearCart}>Update Cart</button>
                 </div>
               }
+
               {cart.length > 0 &&
                 <div className="mt-8 flex flex-col lg:flex-row lg:justify-between lg:items-start">
                   <div className="flex flex-col lg:flex-row lg:items-center lg:space-x-4">
